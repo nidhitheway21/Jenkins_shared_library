3b83dcd34a27b253d079cef0880361bc98da2e5e
Trifrixx <nidhitheway@gmail.com>
2024-11-12 11:16:58 +0530

    updated docker build file

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,12 +1,8 @@
-Create a shared library file for dockerBuild.groovy
-
-
 def call(String dockerHubUsername, String imageName) {
-    // Build the Docker image
     sh "docker build --build-arg REACT_APP_RAPID_API_KEY=3a8bcc0c2amsh4bbce8fcefb6557p14adcfjsn708f4a8909f -t ${imageName} ."
-     // Tag the Docker image
+    
     sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:latest"
-    // Push the Docker image
+    
     withDockerRegistry([url: 'https://index.docker.io/v1/', credentialsId: 'docker']) {
         sh "docker push ${dockerHubUsername}/${imageName}:latest"
     }
